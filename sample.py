cbb1d03de7fdb7cc8de75cc5e97e5cc550711f59
Sumit Roy <sroy8091@gmail.com>
2019-08-27 15:01:36 +0530

    Added multithreaded test cases

--- sample.py
+++ sample.py
@@ -1,5 +1,6 @@
 from main import LoxAM
 import threading
+import time
 
 
 class SampleContext():
@@ -18,34 +19,37 @@ class SampleContext():
         print(self.name)
 
 
-def func():
-    with LoxAM("Sumit", "sumit") as sc:
+def func(i):
+    print("this is the thread", i)
+    with LoxAM("Sumit", i, thread=i) as sc:
         sc.acquire()
+        print("do something after getting lock for thread {}".format(i))
+        time.sleep(5)
 
     print("End of func")
 
 
-class Celsius(object):
-    def __init__(self, value=0.0):
-        print(type(value))
-        self.value = float(value)
-
-    def __get__(self, instance, owner):
-        print(instance)
-        print(owner)
-        return self.value
-
-    def __set__(self, instance, value):
-        print(instance)
-        print(value)
-        self.value = float(value)
-
-    def __delete__(self, instance):
-        pass
-
-
-class Temperature(object):
-    celsius = Celsius()
+# class Celsius(object):
+#     def __init__(self, value=0.0):
+#         print(type(value))
+#         self.value = float(value)
+#
+#     def __get__(self, instance, owner):
+#         print(instance)
+#         print(owner)
+#         return self.value
+#
+#     def __set__(self, instance, value):
+#         print(instance)
+#         print(value)
+#         self.value = float(value)
+#
+#     def __delete__(self, instance):
+#         pass
+#
+#
+# class Temperature(object):
+#     celsius = Celsius()
 
 # t = Temperature()
 
@@ -54,9 +58,10 @@ def main(i):
     global x
     x = 0
 
-    print("this is thread", i)
-    t1 = threading.Thread(target=func)
-    t2 = threading.Thread(target=func)
+    print("this is iteration", i)
+    t1 = threading.Thread(target=func, args=[1])
+    time.sleep(10)
+    t2 = threading.Thread(target=func, args=[2])
 
     t1.start()
     t2.start()
@@ -66,6 +71,8 @@ def main(i):
 
 
 if __name__ == "__main__":
-    for i in range(4):
+    for i in range(2):
         main(i)
         print("x = {1} after Iteration {0}".format(i, x))
+
+    # func(1)
